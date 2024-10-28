15e0e3152ad7c8e3a4fffbb10385a3eec8ddcfe2
Roman Zuboley <xenusrom@gmail.com>
2024-10-28 13:44:03 +0200

    test 3 deploy

--- app/_components/header/index.tsx
+++ app/_components/header/index.tsx
@@ -1,17 +1,18 @@
-import { Logo } from "./logo"
-import { Title } from "./title"
-import { Actions } from "./actions"
+// import { Logo } from "./logo"
+// import { Title } from "./title"
+// import { Actions } from "./actions"
 
 export const Header = () => {
   return (
     <header className='p-2'>
       <div className='flex items-center p-3 gap-4 container m-auto bg-stone-100 rounded-md shadow-sm'>
-        <Logo />
-        <Title />
+        TODO
+        {/* <Logo /> */}
+        {/* <Title /> */}
 
         <div className='grow' />
 
-        <Actions />
+        {/* <Actions /> */}
       </div>
     </header>
   )
